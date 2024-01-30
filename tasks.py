1e9905e69cb0cf46b534a4f0a14bbb31a6fdb3b4
Ivan Tymoshchuk <timohin2231@gmail.com>
2024-01-30 16:50:31 +0100

    first commit

---
+++ tasks.py
@@ -0,0 +1,44 @@
+# ІМІТАЦІЯ  ПРОДАЖІВ КВИТКІВ НА ЛІТАК
+
+# class Flyight():
+#     def __init__(self, capacity):
+#         self.capacity = capacity  # тут ми створюємо к-ст пасажирів
+#         self.passengers = []  # тут ми створюємо місяце для пасажира
+
+#     def add_passenger(self, name):
+#         if not self.open_seats():  # тут ми перевіряємо на наявність місця
+#             return False
+#         self.passengers.append(name)  # тут ми додаємо пасажира
+#         return True
+
+#     def open_seats(self):
+#         # тут ми перевіряємо чи є місце
+#         return self.capacity - len(self.passengers)
+
+
+# flight = Flyight(3)  # тут ми передаємо максимульну кількість пасажирів
+
+# people = ["Ivan", "Oksana", "Inna", "Anastasia"]
+# for persone in people:
+#     if flight.add_passenger(persone):
+#         print(f"Added {persone} to flight succesfully.")
+#     else:
+#         print(f"No available seat for {persone} ")
+
+
+#  ДЕКОРАТОРИ - це функції які можна використовувати та перевиначати у інших функція
+
+def announce(f):
+    def wrapper():
+        print("About to run the function")
+        f()
+        print("Done with the function")
+    return wrapper
+
+
+@announce
+def hello():
+    print("Hello")
+
+
+hello()
