41fa0d67968ce6e78c9efa3aed7325ae26375b38
Ivan Tymoshchuk <timohin2231@gmail.com>
2024-02-18 18:40:36 +0100

    pers err

--- tasks.py
+++ tasks.py
@@ -356,3 +356,19 @@
 #     print(10 / 0)
 # except Exception as e:
 #     print(e)
+
+# * ------ самостійне створення помилки
+
+
+# def divide_nums(a, b):
+#     if b == 0:
+#         raise ValueError("Second argument can't be 0")
+#     return a / b
+
+
+# try:
+#     divide_nums(10, 0)
+# except ValueError as e:
+#     print(e)
+# # Second argument can't be 0
+# print("Continue...")
