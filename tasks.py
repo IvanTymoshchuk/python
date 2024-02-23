a95b2e40acfb0b1109a8dbdf5d52fe6d88a2ae9d
Ivan Tymoshchuk <timohin2231@gmail.com>
2024-02-23 15:51:18 +0100

    err

--- tasks.py
+++ tasks.py
@@ -233,3 +233,18 @@
 #     answer = input("do you want to continue? (yes/no): ")
 #     if answer == "no":
 #         break
+
+
+#! ---- обробка помилок
+# while True:
+#     try:
+#         num_one = float(input("Please enter number one: "))
+#         num_two = float(input("Please enter number two: "))
+#     except ValueError as e:
+#         print(e)
+#         print("You must enter numbers!")
+#         continue
+#     print(num_one / num_two)
+#     answer = input("Do you want to continue? (yes/no): ")
+#     if answer == "no":
+#         break
